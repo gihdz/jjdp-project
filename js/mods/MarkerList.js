29aca05870deb955b15a4c3ef9be907b2b8d2883
gihdz <gdhblok@gmail.com>
2017-01-22 10:26:10 -0400

    Search in marker list

--- js/mods/MarkerList.js
+++ js/mods/MarkerList.js
@@ -1,17 +1,39 @@
 var MarkerList = React.createClass({
+    getInitialState(){
+        return {
+            markers: [],
+            markerList: [],
+            inputValue: ""
+        }
+
+    },
+    componentWillReceiveProps(nextProps){
+        let {markers} = nextProps;
+        this.setState({markers: markers, markerList: markers}, this.filterList);
+    },
+    handleInputChange(e){
+        let inputValue = e.target.value;        
+        this.setState({inputValue}, this.filterList);        
+    },    
+    filterList(){
+        let {inputValue} = this.state;
+        let markerList = this.state.markers.filter(marker => marker.name.toLowerCase().includes(inputValue.toLowerCase()));
+        this.setState({markerList});
+
+    },
     render(){    
-        let {markers} = this.props;
-        if(markers && markers.length === 0) return false;
-        let markersList = this.props.markers.map(marker => {
+        let markers = this.state.markerList;
+        let markerList = markers.map(marker => {
             return (
         <li data-id={marker.place_id} className="list-group-item marker" onClick={this.props.handleMarkerListClick}>
            {marker.name}  
-        </li>    )
+        </li>)
 
         });
         return <div id="marker-list">
+        <input placeholder="Search Markers" className="input-search-marker"  onChange={this.handleInputChange} value={this.state.inputValue} />
         <ul className="list-group">
-        {markersList}
+        {markerList}
         </ul>        
         </div>
     }
